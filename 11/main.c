471a67e5735695f35cf22f86f40d499d32cd502c
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-11-19 21:39:59 -0400

    Exercicio 11 | Concluido

---
+++ 11/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *entrada, *saida;
+    int i;
+    char c;
+
+    entrada = fopen("entrada.txt", "r");
+    saida = fopen("saida.txt", "w");
+    if(entrada == NULL || saida == NULL) {
+        printf("Erro na abertura do arquivo.\n");
+        exit(1);
+    }
+
+    do{
+        fscanf(entrada, "%c", &c);
+        if(feof(entrada)) break;
+        fprintf(saida, "%c", c);
+    } while(1);
+
+    return 0;
+}
